efea46eb1492580815cfd03bae9943e81be17043
cric96 <cric96@users.noreply.github.com>
2026-05-28 08:05:18 +0000

    deploy: scafi/scafi-web@bdad000a36b5f4e3509bf0122744825a402e7aa4

--- assets/BufferResource-C2MoCCpv.js
+++ assets/BufferResource-C2MoCCpv.js
@@ -1,4 +1,4 @@
-import{w as U,c as g,d as c,u as S,B as _,e as m,E as I,f as p}from"./index-DxOZ0vDl.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,c as g,d as c,u as S,B as _,e as m,E as I,f as p}from"./index-BxazWTtB.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
