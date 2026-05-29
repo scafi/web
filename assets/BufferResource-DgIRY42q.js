7739c8f9c9c1d47f48d71069c8fe1e92807cda1f
cric96 <cric96@users.noreply.github.com>
2026-05-29 06:54:46 +0000

    deploy: scafi/scafi-web@927e222010cf1d0c7b8e2d53e310e7abad517c29

--- assets/BufferResource-DgIRY42q.js
+++ assets/BufferResource-DgIRY42q.js
@@ -1,4 +1,4 @@
-import{w as U,c as g,d as c,u as S,B as _,e as m,E as I,f as p}from"./index-DlMyqzf9.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,c as g,d as c,u as S,B as _,e as m,E as I,f as p}from"./index-D84xBNe9.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
